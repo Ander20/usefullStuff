eb144aa0b89a0c46a2e29400eed138057987b2d3
Ander20 <50732677+Ander20@users.noreply.github.com>
2020-07-13 14:03:20 +0200

    Update pingsweep.sh
    from https://www.commandlinefu.com/commands/view/5299/ping-scanning-without-nmap

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 (prefix="10.10.10" && for i in `seq 254`; do (sleep 0.5 && ping -c1 -w1 $prefix.$i &> /dev/null && arp -n | awk ' /'$prefix'.'$i' / { print $1 " " $3 } ') & done; wait)
